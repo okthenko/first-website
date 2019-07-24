e77a63df9579b34864e119a840890b1d40534be5
Parisa Tork <47482049+ParisaTork@users.noreply.github.com>
2019-07-24 23:17:28 +0100

    Delete question_2_spec.rb

--- question_2_spec.rb
+++
@@ -1,6 +0,0 @@
-describe "Q2. Arithmetic" do
-  it "`puts`es result of multiplying `TWELVE` by `FOURTEEN` by `SIXTEEN`" do
-    expect { load "questions/question_2.rb" }
-      .to output(/2688/).to_stdout
-  end
-end
